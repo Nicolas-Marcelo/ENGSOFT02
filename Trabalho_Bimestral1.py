517d88eb461cdc7ad5af3b7c4d811dc6c05b4fd0
Nicolas-Marcelo <86321896+Nicolas-Marcelo@users.noreply.github.com>
2023-09-06 15:12:50 -0300

    Update Trabalho_Bimestral1.py

--- Trabalho_Bimestral1.py
+++ Trabalho_Bimestral1.py
@@ -1,3 +1,5 @@
+import os
+
 # No plano inicial o software vai ser algo para me ajudar no serviço
 # Com cadastramento de fotos por um sistema classificando direto a hora que foi tirada a foto
 # De que obra ela pertence e demais coisas das fotos
@@ -6,19 +8,15 @@
 def boas_vinda() :
     print("Seja bem vindo ao software de auxilio ao auxiliar administrativo da Dalba!")
     print("Dentro do software temos varias funçoes, digite o numero correspondente da função desejada!\n"
-          "( 1 ) Para anexar as fotos!"
-          "( 2 ) Para conferição de funcionarios!"
-          "( 3 ) Para conferição de equipamentos!")
-
-
-
-
-
+          "( 1 ) Para anexar as fotos!\n"
+          "( 2 ) Para conferição de funcionarios!\n"
+          "( 3 ) Para conferição de equipamentos!\n")
+    
+def pausa():
+    os.system("cls")
 
 # Esta variavel armazena o numero da função desejada
 função_desejada = int(input("Digite o numero da função desejada: "))
-
-
 
 
 
@@ -31,6 +29,8 @@ while True:
     
     # Classifica a função que vai ser vista
     função_desejada
+    
+    pausa()
 
     # Primeiro IF para cadastramento de fotos 
     if função_desejada == 1:
@@ -65,7 +65,7 @@ while True:
             contagem = int(input("Digite quantas fotos você quer anexar: "))
             print(f'Você salvara {contagem} fotos!')
             
-            i = 0
+            i = 1
             
             while i <= contagem:
                 nome_foto = input("Digite o nome da foto")
@@ -79,6 +79,8 @@ while True:
             
             for i in fotos_OB0820:
                 print(f'Estas são as fotos: {i}')
+                
+            break
             
         elif obra == 2:
             print("Digite o nome das fotos desejadas para anexar!")
@@ -90,23 +92,26 @@ while True:
             contagem = int(input("Digite quantas fotos você quer anexar: "))
             print(f'Você salvara {contagem} fotos!')
             
-            i = 0
+            i = 1
             
             while i <= contagem:
-                nome_foto = input("Digite o nome da foto")
+                nome_foto = input("Digite o nome da foto: ")
                 print(f'Este é o nome da foto {nome_foto}!')
                 
                 # Adicionando foto
                 fotos_OB0821.append(nome_foto)
+                i = i + 1
             
             # Mostrando fotos salvas
             print("Essas foram as fotos salvas!")
             
             for i in fotos_OB0821:
-                print(f'Estas são as fotos: {i}')
+                print(f'Estas são as fotos: {fotos_OB0821}')
+            
+            break
         
         elif obra == 3:
-            print("Digite o nome das fotos desejadas para anexar!")
+            print("[]")
             
             # Lista com as fotos
             fotos_OB0825 = []
@@ -115,7 +120,7 @@ while True:
             contagem = int(input("Digite quantas fotos você quer anexar: "))
             print(f'Você salvara {contagem} fotos!')
             
-            i = 0
+            i = 1
             
             while i <= contagem:
                 nome_foto = input("Digite o nome da foto")
@@ -123,12 +128,15 @@ while True:
                 
                 # Adicionando foto
                 fotos_OB0825.append(nome_foto)
+                i = i + 1
             
             # Mostrando fotos salvas
             print("Essas foram as fotos salvas!")
             
             for i in fotos_OB0825:
                 print(f'Estas são as fotos: {i}')
+            
+            break
         
         elif obra == 4:
             print("Digite o nome das fotos desejadas para anexar!")
@@ -140,7 +148,7 @@ while True:
             contagem = int(input("Digite quantas fotos você quer anexar: "))
             print(f'Você salvara {contagem} fotos!')
             
-            i = 0
+            i = 1
             
             while i <= contagem:
                 nome_foto = input("Digite o nome da foto")
@@ -148,12 +156,15 @@ while True:
                 
                 # Adicionando foto
                 fotos_OB0826.append(nome_foto)
+                i = i + 1
             
             # Mostrando fotos salvas
             print("Essas foram as fotos salvas!")
             
             for i in fotos_OB0826:
                 print(f'Estas são as fotos: {i}')
+                
+            break
             
         elif obra == 5:
             print("Digite o nome das fotos desejadas para anexar!")
@@ -165,14 +176,15 @@ while True:
             contagem = int(input("Digite quantas fotos você quer anexar: "))
             print(f'Você salvara {contagem} fotos!')
             
-            i = 0
+            i = 1
             
             while i <= contagem:
-                nome_foto = input("Digite o nome da foto")
+                nome_foto = input("Digite o nome da foto: ")
                 print(f'Este é o nome da foto {nome_foto}!')
                 
                 # Adicionando foto
                 fotos_OB0794.append(nome_foto)
+                i = i + 1
             
             # Mostrando fotos salvas
             print("Essas foram as fotos salvas!")
@@ -180,10 +192,11 @@ while True:
             for i in fotos_OB0794:
                 print(f'Estas são as fotos: {i}')
                 
+            break
+                
         else : 
-            pass
-        
-        
+            print("Digite um numero que pertence a lista!")   
+    
     elif função_desejada == 2:
         print("Você selecionou a opção de verificação de funcionarios!")
         
@@ -191,13 +204,9 @@ while True:
         lista = int(input("Digite a OB desejada: "))
         print("Selecionada a OB", lista)
         
-        OB0820 = OB0820
-        OB0821 = OB0821
-        OB0825 = OB0825
-        OB0826 = OB0826
-        OB0794 = OB0794
+        pausa()
         
-        if lista == OB0820:
+        if lista == 820:
             print("Funiconarios da OB0820:")
             
             funcionarios_OB0820 = { "Antonio" : "Rasteleiro",
@@ -206,7 +215,7 @@ while True:
                                     "Jair" : "Servente",
                                     "João Paulo" : "Rasteleiro",
                                     "Pedro" : "Encarregado de obras",
-                                    "Dirceu" : "Mororista",
+                                    "Dirceu" : "Motorista",
                                     "Tiago" : "Apontador",
                                     "Irineu" : "Topografico"}
             
@@ -214,12 +223,17 @@ while True:
             valores = funcionarios_OB0820.values()
             
             # Apresentando os funcionarios 
+            
+            i = 1
+            
             for chaves, valores in funcionarios_OB0820.items():
-                i = 1
-                print(f'({i}) Funionario {chaves} com a função de {valores}')
-                i = i +1
                 
-        elif lista == OB0821:
+                print(f'({i}) Funionario {chaves} com a função de {valores}')
+                i = i + 1
+                
+            break
+                
+        elif lista == 821:
             print("Funionarios da OB0821: ")
             
             funcionarios_OB0821 = { "Antonio" : "Rasteleiro",
@@ -235,13 +249,15 @@ while True:
             chaves = funcionarios_OB0821.keys()
             valores = funcionarios_OB0821.values()
             
+            i = 1
+            
             # Apresentando os funcionarios 
             for chaves, valores in funcionarios_OB0821.items():
-                i = 1
-                print(f'({i}) Funionario {chaves} com a função de {valores}')
-                i = i +1
                 
-        elif lista == OB0825:
+                print(f'({i}) Funionario {chaves} com a função de {valores}')
+                i = i + 1
+                
+        elif lista == 825:
             print("Funionarios da OB0825: ")
             
             funcionarios_OB0825 = { "Antonio" : "Rasteleiro",
@@ -257,13 +273,15 @@ while True:
             chaves = funcionarios_OB0825.keys()
             valores = funcionarios_OB0825.values()
             
+            i = 1
+            
             # Apresentando os funcionarios 
             for chaves, valores in funcionarios_OB0825.items():
-                i = 1
-                print(f'({i}) Funionario {chaves} com a função de {valores}')
-                i = i +1
                 
-        elif lista == OB0826:
+                print(f'({i}) Funionario {chaves} com a função de {valores}')
+                i = i + 1
+                
+        elif lista == 826:
             print("Funionarios da OB0826: ")
             
             funcionarios_OB0826 = { "Antonio" : "Rasteleiro",
@@ -279,13 +297,20 @@ while True:
             chaves = funcionarios_OB0826.keys()
             valores = funcionarios_OB0826.values()
             
+            i = 1
+            
             # Apresentando os funcionarios 
             for chaves, valores in funcionarios_OB0826.items():
-                i = 1
-                print(f'({i}) Funionario {chaves} com a função de {valores}')
-                i = i +1
                 
-        elif lista == OB0794:
+                print(f'({i}) Funionario {chaves} com a função de {valores}')
+                i = i + 1
+                
+            funcionario_procurado = input("Digite qual funcionario você está procurando: ")
+            print(f'o funcionario {funcionario_procurado} está na obra e com a função de {funcionarios_OB0826[funcionario_procurado]}!')
+            
+            break
+                
+        elif lista == 794:
             print("Funionarios da OB0794: ")
             
             funcionarios_OB0794 = { "Antonio" : "Rasteleiro",
@@ -301,16 +326,21 @@ while True:
             chaves = funcionarios_OB0794.keys()
             valores = funcionarios_OB0794.values()
             
+            i = 1
+            
             # Apresentando os funcionarios 
             for chaves, valores in funcionarios_OB0794.items():
-                i = 1
+            
                 print(f'({i}) Funionario {chaves} com a função de {valores}')
-                i = i +1
+                i = i + 1
                 
             funcionario_procurado = input("Digite qual funcionario você está procurando: ")
-            print(chaves[funcionario_procurado])
+            print(f'o funcionario {funcionario_procurado} está na obra e com a função de {funcionarios_OB0794[funcionario_procurado]}!')
             
             # Preciso conferir se um item está a lista
+
+        else :
+            print("Digite o indice da OB correto!")
         
         
     elif função_desejada == 3:
@@ -321,6 +351,7 @@ while True:
     
 
 
+    
     
     
     

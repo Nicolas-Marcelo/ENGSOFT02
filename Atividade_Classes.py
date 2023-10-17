8b4cc3a17f8537fac81561986138a0d07c447ce4
Nicolas-Marcelo <86321896+Nicolas-Marcelo@users.noreply.github.com>
2023-10-16 22:13:57 -0300

    Update Atividade_Classes.py

--- Atividade_Classes.py
+++ Atividade_Classes.py
@@ -53,7 +53,45 @@ m1 = Moto("Motoqueiro fantasma", 2000)
 c1.especificar()
 m1.especificar()
 
-# ============================================== ATVIDADE 1 ============================================== 
+# ============================================== ATVIDADE 4 ============================================== 
+class Veiculo():
+    def __init__(self, tipo, velocidade, ano, cor, km_rodado):
+        self.velocidade = velocidade
+        self.tipo = tipo
+        self.ano = ano
+        self.cor = cor
+        self.km_rodado = km_rodado
+
+    def especificar(self):
+        if self.tipo == "Fusca":
+            print(f'Seu veiculo que é um/a {self.tipo} está andando a velocidade {self.velocidade} cavalos')
+
+        if self.tipo == "Motoqueiro fantasma":
+            print(f'Seu veiculo que é um/a {self.tipo} está andando a velocidade {self.velocidade} cilindradas')
+
+    def descricao(self):
+        print(f'Seu veiculo modelo {self.tipo} do ano {self.ano} de cor {self.cor}, está andando a velocidade de {self.velocidade} '
+              f'e está com contador em {self.km_rodado} KMs rodados!')
+
+from Veiculo import Veiculo
+
+class Carro (Veiculo):
+    def __init__(self, tipo, cavalos, ano, cor, km_rodado):
+        super().__init__(tipo, cavalos, ano, cor, km_rodado)
+
+    def descricao(self):
+        print(f'Sua {self.tipo} fabricada no ano {self.ano} tem a cor {self.cor}, a moto tem sua velocidade maxima de {self.velocidade}'
+              f'e hoje atinge o feito de {self.km_rodado}')
+
+from Veiculo import Veiculo
+
+class Moto (Veiculo):
+    def __init__(self, tipo, cilindradas, ano, cor, km_rodado):
+        super().__init__(tipo, cilindradas, ano, cor, km_rodado)
+
+    def descricao(self):
+        print(f'Seu veiculo modelo {self.tipo} fabricada no ano de {self.ano} tem a cor de {self.cor}'
+              f'e já atingiu a velocidade de {self.velocidade} e agora está com {self.km_rodado} KMs rodados')
 # ============================================== ATVIDADE 1 ============================================== 
 # ============================================== ATVIDADE 1 ============================================== 
 # ============================================== ATVIDADE 1 ============================================== 
